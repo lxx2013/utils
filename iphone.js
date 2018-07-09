6605d5452e929525cbb25c8f4a9054b5a958e91b
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-07-09 22:14:48 +0800

    test iphone

--- iphone.js
+++ iphone.js
@@ -9,6 +9,12 @@ var iphone = {};
 			+"rotateX("+Math.round(e.beta)+"deg) "
 			+"rotateY("+Math.round(e.gamma)+"deg)";
 		document.getElementById("arrow_str").innerHTML = `alpha:${Math.round(e.alpha)}<br>beta:${Math.round(e.beta)}<br>gamma:${Math.round(e.gamma)}`;
+
+
+		var arrow2 = document.getElementById("arrow2");
+		arrow2.style.transform = "rotateZ("+Math.round(360-e.alpha)+"deg) "
+			+"rotateX("+Math.round(-e.beta)+"deg) "
+			+"rotateY("+Math.round(-e.gamma)+"deg)";
 	});
 })();
 
