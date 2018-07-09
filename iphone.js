0ebc4770e50e18b139037b6b8457436b53eac278
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-07-09 21:59:55 +0800

    test iphone

--- iphone.js
+++ iphone.js
@@ -1,8 +1,14 @@
-var iphone = {}
-window.addEventListener("deviceorientation", function (e) {
+var iphone = {};
+(function(){
+	var alpha,beta,gamma;
+	var iphone = {}
+	window.addEventListener("deviceorientation", function (e) {
 		[iphone.alpha,iphone.beta,iphone.gamma] = [e.alpha,e.beta,e.gamma];
 		var arrow = document.getElementById("arrow");
-		arrow.style.transform = "rotateZ("+Math.round(e.alpha)+"deg) " 
-								+"rotateX("+Math.round(e.beta)+"deg) "
-								+"rotateY("+Math.round(e.gamma)+"deg)";		
-});
+		arrow.style.transform = "rotateZ("+Math.round(-e.alpha)+"deg) " 
+			+"rotateX("+Math.round(-e.beta)+"deg) "
+			+"rotateY("+Math.round(-e.gamma)+"deg)";
+		document.getElementById("arrow_str").textContent = `alpha:${e.alpha}<br>beta:${e.beta}<br>gamma:${e.gamma}`;
+	});
+})();
+
