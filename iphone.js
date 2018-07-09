bee6375d674c91806de379a172a1f247a6c5452b
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-07-09 22:37:31 +0800

    iphone -el-card

--- iphone.js
+++ iphone.js
@@ -4,17 +4,12 @@ var iphone = {};
 	var iphone = {}
 	window.addEventListener("deviceorientation", function (e) {
 		[iphone.alpha,iphone.beta,iphone.gamma] = [e.alpha,e.beta,e.gamma];
-		var arrow = document.getElementById("arrow");
+		var arrow = document.getElementById("arrow_str");
 		arrow.style.transform = "rotateZ("+Math.round(e.alpha)+"deg) "
 			+"rotateX("+Math.round(e.beta)+"deg) "
 			+"rotateY("+Math.round(e.gamma)+"deg)";
 		document.getElementById("arrow_str").innerHTML = `alpha:${Math.round(e.alpha)}<br>beta:${Math.round(e.beta)}<br>gamma:${Math.round(e.gamma)}`;
 
-
-		var arrow2 = document.getElementById("arrow2");
-		arrow2.style.transform = "rotateZ("+Math.round(360-e.alpha)+"deg) "
-			+"rotateX("+Math.round(-e.beta)+"deg) "
-			+"rotateY("+Math.round(-e.gamma)+"deg)";
 	});
 })();
 
